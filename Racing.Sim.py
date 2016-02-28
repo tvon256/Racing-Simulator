dad1c393f458b4c7afb137d179c7c548a5c521b9
tvon256 <thholten@live.com>
2016-02-28 15:28:51 -0500

    Added files via upload
    Added Car Generator

--- Racing.Sim.py
+++ Racing.Sim.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Car(object):
 	def __ini__ (self, name, top_speed, acceleration, luck):
 		self.name = name
@@ -5,4 +7,6 @@ class Car(object):
 		self.acceleration = acceleration
 		self.luck = luck
 		
-	
+def make_cars (p1, p2):
+	p1_car = Car(p1, randint(1,5), randint(1,5), randint(1,3))
+	p2_car = Car(p2, randint(1,5), randint(1,5), randint(1,3))
