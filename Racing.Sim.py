ca1bd77be4222179cb02016f58c57e34a3a0abb4
tvon256 <thholten@live.com>
2016-02-28 15:22:04 -0500

    Added files via upload
    Added Car Class

---
+++ Racing.Sim.py
@@ -0,0 +1,8 @@
+class Car(object):
+	def __ini__ (self, name, top_speed, acceleration, luck):
+		self.name = name
+		self.top_speed = top_speed
+		self.acceleration = acceleration
+		self.luck = luck
+		
+	
