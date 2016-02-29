3a81a964805fce6b7a1c87cbf1bd3f1bf23b3fea
tvon256 <thholten@live.com>
2016-02-28 21:05:48 -0500

    v 1.0 : Fixed many Bugs, added random events

--- Racing.Sim.py
+++ Racing.Sim.py
@@ -4,7 +4,6 @@
 from random import randint
 import time
 
-distance = randint(1,3)
 
 class Car(object):
 	chance_of_winning = 0
@@ -15,7 +14,93 @@ class Car(object):
 		self.acceleration = acceleration
 		self.luck = luck
 		
+def race_logic(car1, car2):
 	
+	random_events = ["skidded of a cliff", "got hit by oncoming traffic", "got pulled over for speeding"]
+	distance = randint(1,3)
+	car1_score = 0
+	car2_score = 0
+	car1.chance_of_winning = 0
+	car2.chance_of_winning = 0
+	print ("Distance: " + str(distance))
+	if (distance == 1):
+		if car1.acceleration - car2.acceleration >= 1:
+			car1.chance_of_winning = car1.chance_of_winning + 1
+			print(car1.name + " gained 1 point from acceleration")
+			time.sleep(1)
+		elif car2.acceleration - car1.acceleration >= 1:
+			car2.chance_of_winning = car2.chance_of_winning + 1
+			print(car2.name + " gained 1 point from acceleration")
+			time.sleep(1)
+		if (car1.top_speed - car2.top_speed > 3):
+			car1.chance_of_winning = car1.chance_of_winning + 2
+			print(car1.name + " gained 2 points from top speed")
+			time.sleep(1)
+		elif (car2.top_speed - car1.top_speed > 3):
+			car2.chance_of_winning = car2.chance_of_winning + 2
+			print(car2.name + " gained 2 points from top speed")
+			time.sleep(1)
+	if (distance == 2):
+		if car1.acceleration - car2.acceleration >= 2:
+			car1.chance_of_winning = car1.chance_of_winning + 1
+			print(car1.name + " gained 1 point from acceleration")
+			time.sleep(1)
+		elif car2.acceleration - car1.acceleration >= 2:
+			car2.chance_of_winning = car2.chance_of_winning + 1
+			print(car2.name + " gained 1 point from acceleration")
+			time.sleep(1)
+		if (car1.top_speed - car2.top_speed > 2):
+			car1.chance_of_winning = car1.chance_of_winning + 2
+			print(car1.name + " gained 2 points from top speed")
+			time.sleep(1)
+		elif (car2.top_speed - car1.top_speed > 2):
+			car2.chance_of_winning = car2.chance_of_winning + 2
+			print(car2.name + " gained 2 points from top speed")
+			time.sleep(1)
+	if (distance == 3):
+		if car1.acceleration - car2.acceleration > 3:
+			car1.chance_of_winning = car1.chance_of_winning + 2
+			print(car1.name + " gained 2 points from acceleration")
+			time.sleep(1)
+		elif car2.acceleration - car1.acceleration > 3:
+			car2.chance_of_winning = car2.chance_of_winning + 2
+			print(car2.name + " gained 2 points from acceleration")
+			time.sleep(1)
+		if (car1.top_speed - car2.top_speed >= 1):
+			car1.chance_of_winning = car1.chance_of_winning + 1
+			print(car1.name + " gained 1 point from top speed")
+			time.sleep(1)
+		elif (car2.top_speed - car1.top_speed >= 1):
+			car2.chance_of_winning = car2.chance_of_winning + 1
+			print(car2.name + " gained 1 point from top speed")
+			time.sleep(1)
+	
+	if (randint(0,5) == 5):
+		print ("%s %s" % (car1.name, random_events[randint(0,2)]))
+		car1.chance_of_winning = -10
+	elif (randint(0,5) == 0):
+		print ("%s %s" % (car2.name, random_events[randint(0,2)]))
+		car2.chance_of_winning = -10
+	
+	print ("%s's score before luck: %s" % (car1.name, str(car1.chance_of_winning)))
+	print ("%s's score before luck: %s" % (car2.name, str(car2.chance_of_winning)))
+	
+	car1_score = car1.chance_of_winning + randint(1,3) + car1.luck
+	car2_score = car2.chance_of_winning + randint(1,3) + car2.luck
+	
+	print ("%s's score after luck: %s" % (car1.name, str(car1_score)))
+	print ("%s's score after luck: %s" % (car2.name, str(car2_score)))
+	
+	if (car1_score > car2_score):
+		winner = str(car1.name)
+		car1.races_won = car1.races_won + 1
+	elif (car2_score > car1_score):
+		winner = str(car2.name)
+		car2.races_won = car2.races_won + 1
+	else:
+		winner = "tie"
+	return winner
+		
 def main_game ():
 	print ("#############################################################")
 	print("Welcome To Tom's Racing Simulator")
@@ -32,9 +117,10 @@ def main_game ():
 	
 	counter = 1
 	while counter <= 3:
-		print ("Race %s over a distance of %s. Winner: %s" % (str(distance), str(counter), race_logic(p1_car, p2_car)))
-		counter = counter + 1
 		time.sleep(2)
+		print ("Race %s. Winner: %s" % (str(counter), race_logic(p1_car, p2_car)))
+		counter = counter + 1
+		
 		
 	if (p1_car.races_won > p2_car.races_won):
 		print("%s Has Won!" % (str(p1_car.name)))
@@ -43,47 +129,6 @@ def main_game ():
 	else:
 		print ("It was a tie!")
 	
-def race_logic(car1, car2):
-	
-	if (distance == 1):
-		if car1.acceleration - car2.acceleration >= 1:
-			car1.chance_of_winning = car1.chance_of_winning + 1
-		elif car2.acceleration - car1.acceleration >= 1:
-			car2.chance_of_winning = car2.chance_of_winning + 1
-		if (car1.top_speed - car2.top_speed > 3):
-			car1.chance_of_winning + car1.chance_of_winning + 2
-		elif (car2.top_speed - car1.top_speed > 3):
-			car2.chance_of_winning + car2.chance_of_winning + 2
-	if (distance == 2):
-		if car1.acceleration - car2.acceleration >= 2:
-			car1.chance_of_winning = car1.chance_of_winning + 1
-		elif car2.acceleration - car1.acceleration >= 2:
-			car2.chance_of_winning = car2.chance_of_winning + 1
-		if (car1.top_speed - car2.top_speed > 2):
-			car1.chance_of_winning + car1.chance_of_winning + 2
-		elif (car2.top_speed - car1.top_speed > 2):
-			car2.chance_of_winning + car2.chance_of_winning + 2
-	if (distance == 3):
-		if car1.acceleration - car2.acceleration > 3:
-			car1.chance_of_winning = car1.chance_of_winning + 2
-		elif car2.acceleration - car1.acceleration > 3:
-			car2.chance_of_winning = car2.chance_of_winning + 2
-		if (car1.top_speed - car2.top_speed >= 1):
-			car1.chance_of_winning + car1.chance_of_winning + 1
-		elif (car2.top_speed - car1.top_speed >= 1):
-			car2.chance_of_winning + car2.chance_of_winning + 1
-	
-	car1_score = car1.chance_of_winning + randint(1,3) + car1.luck
-	car2_score = car2.chance_of_winning + randint(1,3) + car2.luck
-	
-	if (car1_score > car2_score):
-		winner = str(car1.name)
-		car1.races_won = car1.races_won + 1
-	elif (car2_score > car1_score):
-		winner = str(car2.name)
-		car2.races_won = car2.races_won + 1
-	else:
-		winner = "tie"
-	return winner
+
 while (1 == 1):			
 	main_game()
